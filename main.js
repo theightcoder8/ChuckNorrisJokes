f575b4ada205dc985d0243947f87ff9bbd2e5e5b
Farid Iskandarov <iskandarovfarid8@gmail.com>
2022-08-01 22:27:08 +0300

    Update

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 document.querySelector('.get-jokes').addEventListener('click', getJokes);
+
+// getJokes 
 function getJokes(e) {
     const number = document.querySelector('input, [type="number"]').value;
+
     const xhr = new XMLHttpRequest();
+    
+    // open
     xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
-   
+    // onload
     xhr.onload = function() {
         if(this.status === 200) {
             const response = JSON.parse(this.responseText);
             let output = '';
-
+            
             if(response.type === 'success') {
                 response.value.forEach(function(joke) {
                     output += `<li>${joke.joke}</li>`;
